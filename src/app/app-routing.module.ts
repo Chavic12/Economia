06b0eec52ec06deb0ab0e4245f462f6bdd24ad1f
Xavier Chavez <xavierchavez916@gmail.com>
2023-01-25 16:01:47 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { UploadFileComponent } from './pages/upload-file/upload-file.component';
+import { EconomicoComponent } from './pages/economico/economico.component';
+import { SocialComponent } from './pages/social/social.component';
+import { AmbientalComponent } from './pages/ambiental/ambiental.component';
+import { InstitucionalComponent } from './pages/institucional/institucional.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { QuienesComponent } from './pages/quienes/quienes.component';
+import { PagesComponent } from './pages/pages.component';
+import { GraficaComponent } from './pages/grafica/grafica.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+
+      { path: 'upload', component: UploadFileComponent },
+      { path: 'economico', component: EconomicoComponent },
+      { path: 'social', component: SocialComponent },
+      { path: 'ambiental', component: AmbientalComponent },
+      { path: 'institucional', component: InstitucionalComponent },
+      { path: 'contactos', component: ContactosComponent },
+      { path: 'quienes', component: QuienesComponent },
+      { path: 'grafica', component: GraficaComponent },
+      { path: 'home', component: HomeComponent },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NopagefoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AmbientalComponent } from './pages/ambiental/ambiental.component';
+import { UploadFileComponent } from './pages/upload-file/upload-file.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { QuienesComponent } from './pages/quienes/quienes.component';
+import { PagesComponent } from './pages/pages.component';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+
+    FooterComponent,
+    AmbientalComponent,
+    UploadFileComponent,
+    HomeComponent,
+    ContactosComponent,
+    QuienesComponent,
+    PagesComponent,
+
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
